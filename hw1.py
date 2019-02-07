6ee68f96cca04d5a7038ee2a35afa745e1a23af0
rameshrallapalli <44247646+rameshrallapalli@users.noreply.github.com>
2019-02-07 13:37:21 -0500

    Add files via upload

--- hw1.py
+++ hw1.py
@@ -16,12 +16,21 @@ x = np.arange(0,2*np.pi,0.1)
 y = np.sin(x)
 z = np.cos(x)
 
+#Adding Tangent for Assignment - Venkata
+t = np.tan(x)
+#Creating a Top level container for all plot elements - Venkata
+fig = plot.figure()
+
 plot.grid(True, which='both')
 plot.axhline(y = 0, color='k')
 
-plot.title('Sine and Cosine')
+plot.title('Sine and Cosine and Tangent')
 plot.xlabel('Time')
 plot.ylabel('Amplitude')
-plot.plot(x,y,x,z)
+plot.plot(x,y,x,z,x,t)
 plot.show()
+
+#Saving the graph localli - venkata
+# we can set the path here based on where we wanted to save- by default it save in same location where the python script exist- venkata
+fig.savefig('sine_cosine_tangent.png')
 
